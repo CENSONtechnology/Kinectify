486231c99db5559f60f2871009d3f391d62eea0f
Mohammed A. Ra'ouf <mohammed.raouf.m@gmail.com>
2017-02-13 07:59:45 +0200

    Added navigation to buttons Added Animation to select buttions

---
+++ Assets/Scripts/NavigateScenes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigateScenes : MonoBehaviour {
+
+    //function to change main camera to another scene which in the build setting 
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName); 
+    }
+}
