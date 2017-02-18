129740d55ffe93da2aa8afed8df7ce912259228b
Mohammed A. Ra'ouf <mohammed.raouf.m@gmail.com>
2017-02-18 03:23:18 +0200

    Splash Screen Changes : - Splash Screen button removed - Auto navigatie to "Home Page" added

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour {
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3f);
+        Application.LoadLevel("Home Page");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
