21efa4682ef4123a35679c0cc9dfa4ac5c703b89
Mohamed <mohamed_emadhassan@hotmail.com>
2017-05-07 16:04:35 +0200

    Edit By mohamdy 1-Add Insert Patient information to the data base 2- show the patient information to the report scene 3-pathing variables from C# script in a scene to another C# script in another scene
    Edit by mohamady

---
+++ Assets/Scripts/mysql/GetMyID.cs
@@ -0,0 +1,7 @@
+﻿
+
+public class GetMyID  {
+    static public string LoginID;
+
+	
+}
---
+++ Assets/Scripts/mysql/GetPatientInformation.cs
@@ -0,0 +1,15 @@
+﻿
+
+public class GetPatientInformation  {
+    public static string Firstname;
+    public static string Lastname; 
+    public static string Age;
+    public static string Gender;
+    public static string Notes;
+    public static string Password;
+    public static string Phone;
+    public static string Email;
+
+
+
+}
